eb4d0b94877b3418b6b63af070a03bd08d3531bf
Shubham <shubhamsm4175@gmail.com>
2022-10-01 15:48:16 +0530

    map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import './App'
 import React from 'react'
-import {Alert} from 'react-bootstrap'
+ import {Table} from 'react-bootstrap'
 function App(){
+    // const students = [1,2,3,4,5]
+  const students = [
+    {name:'Shubham',rollno:12},
+    {name:'Borly',rollno:13},
+    {name:'Goku',rollno:14},
+    {name:'Bardock',rollno:15},
+    {name:'Vegeta',rollno:16},
+  ]
   return(
     <div className="App"> 
-      <h1>Bootstrap</h1>
-        <Alert  variant="warning">
-          This is a   alert—check it out!
-        </Alert>
+      <h1>Array in React</h1>
+      <Table striped bordered hover>
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Roll number</th>
+        </tr>
+        </thead>
+        <tbody>
+            {
+              students.map((item,index)=>
+                <tr key={index}>
+                  <td>{item.name}</td>
+                  <td>{item.rollno}</td>
+                </tr>
+              )
+            }
+        </tbody>
+     </Table>
     </div>
   )
 }
